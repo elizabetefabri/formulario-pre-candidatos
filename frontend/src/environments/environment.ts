c3945f79060ce0649db2f317b7a01e2884ae9755
Elizabete Fabri <elizabete958@gmail.com>
2024-08-24 18:47:26 -0300

    conexao

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  CONNECTION_URL:'https://backend-formulario-precadastro.vercel.app/',
+  API:'https://backend-formulario-precadastro.vercel.app/api/data'
+};
