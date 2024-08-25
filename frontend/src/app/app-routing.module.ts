477cd6dfe0b4d33ba6aaa2fe764e77c12c8977bc
Elizabete Fabri <elizabete958@gmail.com>
2024-08-24 22:16:48 -0300

    add route de form

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateDataComponent } from './Layout/Components/create-data/create-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'formulario', pathMatch: 'full' },
+  { path: 'formulario', component: CreateDataComponent},
+  { path: '**', redirectTo: '/formulario' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
