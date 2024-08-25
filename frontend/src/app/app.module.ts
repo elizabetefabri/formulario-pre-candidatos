bb2ecebaca70c4dd90628404217962ce74fbe385
Elizabete Fabri <elizabete958@gmail.com>
2024-08-24 23:03:09 -0300

    importação dos steps em module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,11 +11,25 @@ import { AppComponent } from './app.component';
 import { ComponentsModule } from './Layout/Shared/ComponentsMaterial/components.module';
 import {MatStepperIntl} from '@angular/material/stepper';
 import { CreateDataComponent } from './Layout/Components/create-data/create-data.component';
+import { Step01Component } from './Layout/Components/create-data/step-01/step-01.component';
+import { Step02Component } from './Layout/Components/create-data/step-02/step-02.component';
+import { Step03Component } from './Layout/Components/create-data/step-03/step-03.component';
+import { Step04Component } from './Layout/Components/create-data/step-04/step-04.component';
+import { Step05Component } from './Layout/Components/create-data/step-05/step-05.component';
+import { Step06Component } from './Layout/Components/create-data/step-06/step-06.component';
+import { Step07Component } from './Layout/Components/create-data/step-07/step-07.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateDataComponent
+    CreateDataComponent,
+    Step01Component,
+    Step02Component,
+    Step03Component,
+    Step04Component,
+    Step05Component,
+    Step06Component,
+    Step07Component
   ],
   imports: [
     BrowserModule,
