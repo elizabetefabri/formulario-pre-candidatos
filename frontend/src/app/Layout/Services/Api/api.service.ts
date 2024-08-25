8b8056cd35053e49252c4a4748779663cc0aa87d
Elizabete Fabri <elizabete958@gmail.com>
2024-08-24 23:00:20 -0300

    alter link do banckend

--- frontend/src/app/Layout/Services/Api/api.service.ts
+++ frontend/src/app/Layout/Services/Api/api.service.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+
 import { UserSheet } from '../../Models/UserSheet';
+import { environment } from '../../../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
-  private apiUrl = `${environment.API}`;
+export class ApiService implements OnInit{
+  private apiUrl = 'https://backend-formulario-precadastro.vercel.app/api/data';
 
   constructor(private http: HttpClient) { }
+  ngOnInit(): void {  }
 
   getData(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
