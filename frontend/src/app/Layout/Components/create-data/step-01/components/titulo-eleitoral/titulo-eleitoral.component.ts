a391923d3deac5e88c081893e600cf5daecafbb5
Elizabete Fabri <elizabete958@gmail.com>
2024-09-01 00:26:22 -0300

    Add componente de titulo e sua validações

--- frontend/src/app/Layout/Components/create-data/step-01/components/titulo-eleitoral/titulo-eleitoral.component.ts
+++ frontend/src/app/Layout/Components/create-data/step-01/components/titulo-eleitoral/titulo-eleitoral.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { tituloEleitoralValidator } from 'src/app/Validators/titulo-eleitoral.validator';
 
 @Component({
   selector: 'app-titulo-eleitoral',
   templateUrl: './titulo-eleitoral.component.html',
   styleUrls: ['./titulo-eleitoral.component.css']
 })
-export class TituloEleitoralComponent {
+export class TituloEleitoralComponent implements OnInit {
+  @Input() formGroup!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup.addControl('tituloEleitoral', this.fb.control('', [Validators.required, tituloEleitoralValidator()]));
+  }
 }
